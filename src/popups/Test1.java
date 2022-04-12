144e0be2967f89f2a7bfcc1bade9e29c71f9749e
QSP <QSP@QSP>
2022-04-12 19:11:42 +0530

    popups

---
+++ src/popups/Test1.java
@@ -0,0 +1,25 @@
+package popups;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.makemytrip.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+driver.findElement(By.xpath("//label[@for='departure']")).click();
+WebElement ele = driver.findElement(By.xpath("(//p[text()='12'])[2]"));
+System.out.println(ele.getText());
+ele.click();
+	
+	}
+
+}
---
+++ src/popups/Test3.java
@@ -0,0 +1,38 @@
+package popups;
+
+import java.time.Duration;
+import java.util.Set;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.selenium.dev/downloads/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		//new tab
+		driver.switchTo().newWindow(WindowType.TAB);
+		
+		//new window
+		driver.switchTo().newWindow(WindowType.WINDOW);
+		
+		driver.get("https://www.amazon.com/");
+		
+		String parent = driver.getWindowHandle();
+		System.out.println(parent);
+		
+		Set<String> child = driver.getWindowHandles();
+		System.out.println(child);
+		
+		driver.quit();
+		
+	}
+
+}
