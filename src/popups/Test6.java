d887bba94b72e8df8d5bdd065838a621d6125d79
QSP <QSP@QSP>
2022-04-16 19:03:43 +0530

    Popups

---
+++ src/popups/Test6.java
@@ -0,0 +1,8 @@
+WinWaitActive("Open")
+Sleep(3000)
+ControlFocus("Open","","Edit1")
+Sleep(3000)
+Send("C:\Users\QSP\Desktop\appiumInterview.docx")
+Sleep(2000)
+ControlClick("Open","&Open","Button1")
+
